0b14900f9038e1a965c81acdaee29750bb047400
sai koushik <sai koushik>
2016-12-18 22:18:47 +0530

    assignment1.2

---
+++ src/add.java
@@ -0,0 +1,17 @@
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;
+		int b= a-- - --a; 
+		int c=a--; 
+		int d=a>>2; 
+		int e=a&b;
+		System.out.println("value is"+b);
+		System.out.println("value is"+c);
+		System.out.println("value is"+d);
+		System.out.println("value is"+e);
+	}
+
+}
